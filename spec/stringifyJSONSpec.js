d57e06cdbdfc6a0cb13a73f03a3b78b0d72ddf39
theptrk <patrick.tran06@gmail.com>
2014-03-24 11:57:01 -0700

    fixed!

--- spec/stringifyJSONSpec.js
+++ spec/stringifyJSONSpec.js
@@ -2,13 +2,13 @@
 describe("stringifyJSON", function(){
   it("should match the result of calling JSON.stringify", function(){
 
-    arrayWithValidElements.forEach(function(obj){
+    stringifiableValues.forEach(function(obj){
       var result = stringifyJSON(obj);
       var expected = JSON.stringify(obj);
       expect(result).toEqual(expected);
     });
 
-    objectWithInvalidAttributes.forEach(function(obj){
+    nonStringifiableValues.forEach(function(obj){
       var result = stringifyJSON(obj);
       var expected = JSON.stringify(obj);
       expect(result).toEqual(expected);
